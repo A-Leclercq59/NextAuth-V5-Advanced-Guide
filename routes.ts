5145ab182d5139b2d08aa58023bf6d8e9495a198
Anthony Leclercq <aleclercq59.dev@gmail.com>
2024-01-07 13:04:43 +0100

    Reset Password Token

--- routes.ts
+++ routes.ts
@@ -10,7 +10,12 @@ export const publicRoutes = ["/", "/auth/new-verification"];
  * Theses routes will redirect logged is users to /settings
  * @type {string[]}
  */
-export const authRoutes = ["/auth/login", "/auth/register", "/auth/error"];
+export const authRoutes = [
+  "/auth/login",
+  "/auth/register",
+  "/auth/error",
+  "/auth/reset",
+];
 
 /**
  * The prefix for API authentication routes
